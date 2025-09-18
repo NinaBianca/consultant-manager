3d1788a4cb9a8752bd1cd2d91620122018d3aa99
NinaBianca <100276915+NinaBianca@users.noreply.github.com>
2025-09-18 18:36:48 +0200

    JPA Class Annotations added

--- manager/src/main/java/com/consultancy/manager/entities/ProjectRequirement.java
+++ manager/src/main/java/com/consultancy/manager/entities/ProjectRequirement.java
@@ -1,9 +1,19 @@
 package com.consultancy.manager.entities;
 
+import jakarta.persistence.*;
+
 import java.util.List;
 
+@Entity
+@Table(name="PROJECTS")
 public class ProjectRequirement {
+    @Id
+    @GeneratedValue()
+    private Integer id;
 
+    @Column(name="DESCRIPTION")
     private String projectDescription;
+
+
     private List<Skill> skills;
 }
